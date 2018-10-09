97ee500193b2d11b24cf8ec5005e2b7e88fe6c6f
Adrian Wells <awells@syseng.com>
2018-10-09 14:41:08 -0400

    Minor edits

--- ITGlueAPI/Tests/ModuleSettings.Tests.ps1
+++ ITGlueAPI/Tests/ModuleSettings.Tests.ps1
@@ -7,19 +7,18 @@ $ThisFileName = $ThisFile | Split-Path -Leaf
 Describe "Tests" {
     Context "Test $ThisFileName Functions" {
         It "Export-ITGlueModuleSettings should export config.psd1 file" {
-            # Test APIkey test
-            Add-ITGlueAPIKey -Api_Key 'APIkeytest'
-
+            # Test APIkey test by creating some test API key data
+            Add-ITGlueAPIKey -Api_Key 'APIkeytestexample'
             Export-ITGlueModuleSettings
             $outputPath = "$($env:USERPROFILE)\ITGlueAPI"
             $outputPath+"\config.psd1" | Should -Exist
         }
         It "Import-ITGlueModuleSettings should import configuration and populate variables" {
-            # Test for presence of variable and remove for basic and incomplete test import process 
+            # Test for presence of variable and remove it to allow for basic and incomplete test import process 
             if ($ITGlue_API_Key) {
                 Remove-ITGlueAPIKey
             }
-            Import-ITGlueModuleSettings        
+            Import-ITGlueModuleSettings
             Get-ITGlueAPIKey | Should -Not -BeNullOrEmpty
             $ITGlue_JSON_Conversion_Depth | Should -Not -BeNullOrEmpty
             $ITGlue_Base_URI | Should -Not -BeNullOrEmpty
